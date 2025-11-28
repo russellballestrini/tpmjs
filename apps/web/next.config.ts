b93cd42167d7ccc5372dd0dab0f4f394721c6542
Ajax Davis <thomasalwyndavis@gmail.com>
2025-11-28 20:36:47 +1000

    fix: remove redirects to resolve redirect loop

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   transpilePackages: ['@tpmjs/ui', '@tpmjs/utils', '@tpmjs/db', '@tpmjs/types', '@tpmjs/env'],
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/((?!api).*)',
-        has: [
-          {
-            type: 'host',
-            value: 'www.tpmjs.com',
-          },
-        ],
-        destination: 'https://tpmjs.com/$1',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
